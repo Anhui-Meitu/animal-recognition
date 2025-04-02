9476891ebb676882b7e433d7931823ce0767012a
卢沛恒 <15421215+LuPeiheng@user.noreply.gitee.com>
2025-04-02 09:16:48 +0800

    included yolo models

---
+++ src/models.py
@@ -0,0 +1,3 @@
+"""
+list of available models
+"""
---
+++ src/train.py
@@ -0,0 +1,23 @@
+"""
+Trains a model using the specified configuration file.
+用于训练模型的脚本。
+Designed to run from the command line.
+设计为从命令行运行。
+"""
+
+import os
+import sys
+import argparse
+
+import torch
+from ultralytics import YOLO
+
+model = YOLO("yolo11l.pt")
+model.train(
+    data = '/mnt/e/wfs-dataset/data.yaml',
+    epochs = 200,
+    imgsz = 736,
+    batch = 16,
+    device = 0,
+    save_dir = '/mnt/e/wfs-dataset/weights',
+)
